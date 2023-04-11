8ccd2a23c99f473c829dbfece94e335ae7a0a665
kathleenminor <kaathmin@gmail.com>
2023-04-11 11:21:55 +0200

    distance aumentato

--- config/scout.php
+++ config/scout.php
@@ -122,7 +122,7 @@ return [
         'fuzzy' => [
             'prefix_length' => 2,
             'max_expansions' => 50,
-            'distance' => 2
+            'distance' => 5
         ],
         'asYourType' => false,
         'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
