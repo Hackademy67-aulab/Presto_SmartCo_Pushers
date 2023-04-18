e915feee6a7f2cc8d2739107508e8ba68cf5f166
fabio massa <fabioalessandromassa@gmail.com>
2023-04-18 14:46:47 +0200

    migliorato sezione aggiungi annuncio e resa responsive

--- resources/views/createAds.blade.php
+++ resources/views/createAds.blade.php
@@ -1,16 +1,10 @@
 <x-layout>
-<div>
-<div class="container vh-100 d-flex flex-column align-items-center justify-content-center">
-    <div class="row">
+    <div>
+        <section class="p-5 d-flex justify-content-center" style="font-size:1.2rem; position:relative; top:5rem"><a class="text-secondary" style="text-decoration: none" href="{{ route('homePage') }}">Home</a><span class="mx-3 text-secondary">></span><p class="m-0 p-0 text-dark">Inserisci annuncio</p></section>
 
 
-        <div class="col-12 ">
-            <section class="p-5 d-flex justify-content-center " style="font-size:1.2rem; margin-top:5rem"><a class="text-secondary" style="text-decoration: none" href="{{ route('homePage') }}">Home</a><span class="mx-3 text-secondary">></span><p class="m-0 p-0 text-dark">Inserisci annuncio</p></section>
-        <livewire:create-ad-form />
-
+                <livewire:create-ad-form />
 
         </div>
-    </div>
-</div>
-</div>
-</x-layout>
+
+    </x-layout>
